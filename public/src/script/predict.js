671a18b90810b92e1ec50901bf7a6ebbd50f2c77
yourbeagle <mainwattson3135@gmail.com>
2022-12-05 20:32:16 +0700

    add predict feature

--- public/src/script/predict.js
+++ public/src/script/predict.js
@@ -1,17 +1,40 @@
+const filenameSpan = document.getElementById("filename-span")
+const fileInput = document.getElementById("file-upload")
+const fileBtn = document.getElementById("label-file")
+const wrapcon = document.getElementById("wrapped-container")
+const wrapup = document.getElementById("wrap-upload")
 let imageLoaded = false;
+
 $("#file-upload").change(function () {
 	imageLoaded = false;
 	let reader = new FileReader();
 	reader.onload = function () {
 		let dataURL = reader.result;
-		$("#file-upload").attr("src", dataURL);
+		$("#selected-image").attr("src", dataURL);
 		$("#prediction-list").empty();
 		imageLoaded = true;
 	}
 	
 	let file = $("#file-upload").prop('files')[0];
+	
 	reader.readAsDataURL(file);
+
+	if ($("#file-upload")[0].files[0] != undefined){
+		confirm("Pastikan yang anda upload adalah foto CT Scan Anda")
+	}
 });
+
+fileInput.addEventListener("change", updateDisplay)
+
+function updateDisplay(){
+	const filename = fileInput.files[0]?.name;
+	filenameSpan.textContent = filename || "(choose file)";
+	filenameSpan.classList.remove("hidden")
+	wrapcon.classList.add("hidden")
+	wrapup.classList.add("add-height")
+
+}
+
 
 let model;
 let modelLoaded = false;
@@ -34,7 +57,7 @@ $("#predict-button").click(async function () {
 	if (!modelLoaded) { alert("The model must be loaded first"); return; }
 	if (!imageLoaded) { alert("Please select an image first"); return; }
 	
-	image = $('#file-upload').get(0);
+	image = $('#selected-image').get(0);
 	
 	// Pre-process the image
 	console.log( "Loading image..." );
@@ -50,10 +73,8 @@ $("#predict-button").click(async function () {
 		.map(function (p, i) { // this is Array.map
 			console.log()
 			return {
-				
 				probability: p,
 				className: TARGET_CLASSES[i] // we are selecting the value from the obj
-
 			};
 			
 		}).sort(function (a, b) {
@@ -62,6 +83,7 @@ $("#predict-button").click(async function () {
 
 	$("#prediction-list").empty();
 	result_predict.forEach(function (p) {
-		$("#prediction-list").append(`<li>${p.className}: ${p.probability.toFixed(8)*100 + "%"}</li>`);
+			$("#prediction-list").append(`<li>${p.className}: ${p.probability.toFixed(1)*100 + "%"}</li>`);
 		});
+		$("#predict-button").toggle('show')
 });
