42fb644f73520d6e39db42c5396d03ff9982fd35
yourbeagle <mainwattson3135@gmail.com>
2022-12-09 04:51:31 +0700

    enable loading page...

--- public/assets/script/script.js
+++ public/assets/script/script.js
@@ -1,14 +1,14 @@
-// const footer = document.getElementById('footer-test')
+const footer = document.getElementById('footer-test')
 
-// $('body').append('<div style="" id="loadingDiv"><div class="loader">Loading...</div></div>');
-// $(window).on('load', function(){
-//   footer.classList.add('hidden')
-//   setTimeout(removeLoader, 3000); //wait for page load PLUS two seconds.
-// });
-// function removeLoader(){
-//     $( "#loadingDiv" ).fadeOut(500, function() {
-//       // fadeOut complete. Remove the loading div
-//       footer.classList.remove('hidden')
-//       $( "#loadingDiv" ).remove(); //makes page more lightweight 
-//   });  
-// }
+$('body').append('<div style="" id="loadingDiv"><div class="loader">Loading...</div></div>');
+$(window).on('load', function(){
+  footer.classList.add('hidden')
+  setTimeout(removeLoader, 3000); //wait for page load PLUS two seconds.
+});
+function removeLoader(){
+    $( "#loadingDiv" ).fadeOut(500, function() {
+      // fadeOut complete. Remove the loading div
+      footer.classList.remove('hidden')
+      $( "#loadingDiv" ).remove(); //makes page more lightweight 
+  });  
+}
